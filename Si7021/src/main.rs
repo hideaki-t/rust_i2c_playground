702d9b5d7cd2a9fe195c58f0ac46d9084b1c6ef9
Hideaki Takahashi <mymelo@gmail.com>
2018-06-15 08:50:57 -0400

    add reletive humidity related code

--- Si7021/src/main.rs
+++ Si7021/src/main.rs
@@ -10,6 +10,8 @@ use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 const SI7021_CMD_MEASURE_TEMP_NO_HOLD: u8 = 0xF3;
+const SI7021_CMD_MEASURE_RH_NO_HOLD: u8 = 0xF5;
+const SI7021_CMD_MEASURE_TEMP_AFTER_RH: u8 = 0xE0;
 const SI7021_CMD_RESET: u8 = 0xFE;
 const SI7021_CMD_READ_USER_REG: u8 = 0xE7;
 const SI7021_CMD_READ_FIRMWARE_REV: u16 = 0x84B8;
@@ -71,21 +73,50 @@ fn reset<P: AsRef<Path>>(path: P, addr: u16) -> Result<(), LinuxI2CError> {
     Ok(())
 }
 
+fn calc_temp(buf: [u8; 3]) -> f32 {
+    let temp_raw = (buf[0] as u16) << 8 | buf[1] as u16;
+    175.72 * (temp_raw as f32) / 65536.0 - 46.85
+}
+
+fn calc_rh(buf: [u8; 3]) -> f32 {
+    let rh_raw = (buf[0] as u16) << 8 | buf[1] as u16;
+    125.0 * (rh_raw as f32) / 65536.0 - 6.0
+}
+
 fn read_temp<P: AsRef<Path>>(path: P, addr: u16) -> Result<f32, LinuxI2CError> {
     let mut dev = try!(LinuxI2CDevice::new(path, addr));
     try!(dev.write(&[SI7021_CMD_MEASURE_TEMP_NO_HOLD]));
     thread::sleep(Duration::from_millis(25));
     let mut buf: [u8; 3] = [0; 3];
     try!(dev.read(&mut buf));
-    let temp_raw = (buf[0] as u16) << 8 | buf[1] as u16;
-    let temp = 175.72 * (temp_raw as f32) / 65536.0 - 46.85;
-    Ok(temp)
+    Ok(calc_temp(buf))
+}
+
+fn read_rel_humidity<P: AsRef<Path>>(path: P, addr: u16) -> Result<f32, LinuxI2CError> {
+    let mut dev = try!(LinuxI2CDevice::new(path, addr));
+    try!(dev.write(&[SI7021_CMD_MEASURE_RH_NO_HOLD]));
+    thread::sleep(Duration::from_millis(25));
+    let mut buf: [u8; 3] = [0; 3];
+    try!(dev.read(&mut buf));
+    Ok(calc_rh(buf))
+}
+
+fn read_rel_humidity_and_temp<P: AsRef<Path>>(path: P, addr: u16) -> Result<(f32, f32), LinuxI2CError> {
+    let rh = read_rel_humidity(&path, addr);
+    let mut dev = try!(LinuxI2CDevice::new(path, addr));
+    thread::sleep(Duration::from_millis(25));
+    try!(dev.write(&[SI7021_CMD_MEASURE_TEMP_AFTER_RH]));
+    thread::sleep(Duration::from_millis(25));
+    let mut buf: [u8; 3] = [0; 3];
+    try!(dev.read(&mut buf));
+    calc_temp(buf);
+    Ok((calc_temp(buf), rh.unwrap()))
 }
 
 fn main() {
     dump_info(Path::new("/dev/i2c-0"), 0x40).unwrap();
-    match read_temp("/dev/i2c-0", 0x40) {
-        Ok(temp) => println!("{}", temp),
+    match read_rel_humidity_and_temp("/dev/i2c-0", 0x40) {
+        Ok((t, h)) => println!("temp: {}, relative humidity: {}", t, h),
         Err(err) => println!("err: {:?}", err),
     };
 }
